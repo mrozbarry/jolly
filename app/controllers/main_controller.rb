d9291ade9b4948c7e4e4235c2a7e8e1e35cea305
Alex Barry <alex.barry@gmail.com>
2014-04-12 16:11:22 -0400

    More stylistic changes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,12 @@
 class MainController < ApplicationController
   def index
+
+    @servers = []
+
+    @servers << { :name => "Jolly and Friends Spacebuild #1", :join => "steam://host:port", :status => "online", :players => 5, :collection => "#steam_workshop_collection_url" }
+    @servers << { :name => "Jolly and Friends Spacebuild #2", :join => "steam://host:port", :status => "online", :players => 9, :collection => "#steam_workshop_collection_url" }
+    @servers << { :name => "Jolly and Friends DarkRP", :join => "steam://host:port", :status => "offline", :players => 0, :collection => "#steam_workshop_collection_url" }
+
   end
 
   def motd
