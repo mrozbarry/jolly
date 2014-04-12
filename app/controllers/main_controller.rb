c45ec0d07a36d7b1b35e4b121491c883ddcf3877
Alex Barry <alex.barry@gmail.com>
2014-04-12 00:17:03 -0400

    Initial commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,10 @@
+class MainController < ApplicationController
+  def index
+  end
+
+  def motd
+  end
+
+  def contact
+  end
+end
---
+++ test/controllers/main_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class MainControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get motd" do
+    get :motd
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+end
