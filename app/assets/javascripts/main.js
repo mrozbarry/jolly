40569da1c3d4cc890294d141b5bf5d0b97031255
Alex Barry <alex.barry@gmail.com>
2014-04-14 12:05:15 -0400

    Fixed switching screenshot bug

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,16 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-setInterval( function() {
-  var selected = $(".screenshot.selected");
-  if ( selected.length )
-  {
-    var next = false
-    if ( !$(selected).is(":last") )
-      next = $(selected).next();
-    else
-      next = $(".screenshot.first");
-    $(next).click();
+function screenshotIntervalReset()
+{
+  if ( window.screenshotInterval ) clearInterval( window.screenshotInterval );
+  window.screenshotInterval = setInterval( function() {
+    var selected = $(".screenshot.selected");
+    if ( selected.length )
+    {
+      var next = false
+      if ( !$(selected).is(":last") )
+        next = $(selected).next();
+      else
+        next = $(".screenshot.first");
+      $(next).click();
 
-  }
-}, 6000);
+    }
+  }, 6000);
+}
+screenshotIntervalReset();
