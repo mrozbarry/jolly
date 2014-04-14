1d888435d9f00e9e1187d263a53b3e978bc45c93
Alex Barry <alex.barry@gmail.com>
2014-04-14 01:11:50 -0400

    Added auto-scrolling through images

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(function(){
+setInterval( function() {
+  var selected = $(".screenshot.selected");
+  if ( selected.length )
+  {
+    var next = false
+    if ( !$(selected).is(":last") )
+      next = $(selected).next();
+    else
+      next = $(".screenshot.first");
+    $(next).click();
 
-});
+  }
+}, 6000);
