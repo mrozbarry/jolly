d61cab146374c8df067b6a03cddf6ad6dcf34ff7
Alex Barry <alex.barry@gmail.com>
2014-04-14 12:00:49 -0400

    Added log output to whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,6 @@
 
 # Example:
 #
-# set :output, "/path/to/my/cron_log.log"
 #
 # every 2.hours do
 #   command "/usr/bin/some_great_command"
@@ -18,6 +17,8 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+
+set :output, "log/whenever.log"
 
 every 5.minutes do
 
