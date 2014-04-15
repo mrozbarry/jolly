3791dac81721e2a599e6a5e388531c1a9ef44c51
Alex Barry <alex.barry@gmail.com>
2014-04-14 23:22:05 -0400

    Added highchart assets to precompiler

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,3 +2,4 @@
 
 Rails.application.config.assets.precompile += %w( screenshots/*.jpg )
 Rails.application.config.assets.precompile += %w( amazium/amazium.css amazium/amazium-percent.css amazium/base.css amazium/form.css amazium/layout.css )
+Rails.application.config.assets.precompile += %w( highcharts.gray.js highcharts.js jquery.highchartTable.js )
